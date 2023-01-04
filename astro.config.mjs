ab84043470135a0f56d2d1347215357d3e46fef5
soapkelele <buev_anton@mail.ru>
2023-01-04 18:31:43 +0300

    upd

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
+import compress from 'astro-compress'
+
+// https://astro.build/config
+import preact from '@astrojs/preact'
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), compress(), preact()],
 })
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    './src/**/!(tailwind).{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
   future: {
     hoverOnlyWhenSupported: true,
   },
   theme: {
-    extend: {
-      colors: {
-        black: {
-          background: '#040404',
-          'background-purpose': '#12141D',
-          'perk-gradient-light': 'rgba(4, 6, 14, 0.9)',
-          'perk-gradient-dark': '#04060E',
-        },
-      },
-    },
     container: {
       center: true,
       padding: '1rem',
